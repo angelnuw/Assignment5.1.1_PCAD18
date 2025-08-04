9b5649febc37a38691e6de8fa51c5751a1da8648
Angel Nuw <Angel.Nuw@mssa.newhorizons.com>
2025-08-04 16:26:42 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment5._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check if it is" +
+                "a palindrome or not: ");
+            Console.WriteLine();
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isPalindrome = true;
+            bool notPalindrome = false;
+
+
+            if (number < 0)
+            {
+                Console.WriteLine("A negative number cannot be a palindrome.");
+                return;
+            }
+            int originalNumber = number;
+            int reversedNumber = 0;
+
+            while (number > 0) // Loop to reverse the number
+            {
+                int digit = number % 10; // Get the last digit
+                reversedNumber = (reversedNumber * 10) + digit; // Append it to the reversed number
+                number /= 10; // Remove the last digit from the original number
+            }
+            
+            if (originalNumber == reversedNumber) //check if the original number is a palindrome
+            {
+                Console.WriteLine($"{isPalindrome}");
+            }
+            else
+            {
+                Console.WriteLine($"{notPalindrome}");
+            }
+        }
+    }
+}
